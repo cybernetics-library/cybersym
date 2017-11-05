8f6ce823abde86d03314010781d6f6583650cd55
Dan Taeyoung <provolot@gmail.com>
2017-11-05 00:54:23 -0400

    updating to /pp on an interval

--- app/models/predator_prey/Engine.py
+++ app/models/predator_prey/Engine.py
@@ -9,7 +9,8 @@ from World import World
 
 MONUMENTS_API_URL = "http://localhost:5000/monuments"
 PP_API_URL = "http://localhost:5000/pp"
-INTERVAL_TIME = 1
+UPDATE_INTERVAL_SECS = 1
+PP_POST_INTERVAL_SECS = 5
 
 #######
 
@@ -32,7 +33,6 @@ def update():
 
     # get from api
     this_mstate = requests.get(MONUMENTS_API_URL).json()
-    print(this_mstate)
 
     if(this_mstate != prev_mstate): # if state is different
 
@@ -40,7 +40,7 @@ def update():
         new_relations = Logic.monument_state_to_relations(this_mstate)
 
         # convert relation graphs to PreyWorld
-        PreyWorld.change(relations=new_relations)
+        PreyWorld.replace_edges(new_relations)
 
         # store previous state so we don't do this over and over again.
         # although, in theory, nothing goes wrong if we do.
@@ -49,13 +49,53 @@ def update():
     # update world with relations
     PreyWorld.update()
 
+
 def run():
     setup()
 
+    counter = 0
     starttime=time.time()
     while True:
         update()
-        time.sleep(INTERVAL_TIME - ((time.time() - starttime) % INTERVAL_TIME))
+        counter += 1
+        time.sleep(UPDATE_INTERVAL_SECS - ((time.time() - starttime) % UPDATE_INTERVAL_SECS))
+        if(counter >= (PP_POST_INTERVAL_SECS / UPDATE_INTERVAL_SECS)):
+            counter = 0
+            update_db_with_world_state()
+
+def update_db_with_world_state():
+    PreyWorld.pprint()
+    G = PreyWorld.export_graph()
+    r = requests.post(PP_API_URL, data = G)
+
+
+#######
+
+def setup():
+    global PreyWorld, this_mstate, prev_mstate
+
+    PreyWorld = World()
+
+    # get graph from `/pp
+    try: 
+        ppstate = requests.get(PP_API_URL).json()
+        PreyWorld.import_graph(ppstate)
+    except Exception as e:
+        print(e)
+
+    prev_mstate = {}
+
+def update():
+    global PreyWorld, this_mstate, prev_mstate
+
+    # get from api
+    this_mstate = requests.get(MONUMENTS_API_URL).json()
+
+    if(this_mstate != prev_mstate): # if state is different
+
+        # convert monument state to relation graphs
+        new_relations = Logic.monument_state_to_relations(this_mstate)
+
 
 #######
 if __name__ == "__main__":
--- app/models/predator_prey/World.py
+++ app/models/predator_prey/World.py
@@ -8,24 +8,36 @@ import time
 
 # World treates Ploopy as a library that knows nothing about cybersym.
 
+# this is starting to become a useless wrapper... but keeping here for now.
+
 class World:
 
-    # example relations: {'rabbits->foxes': 0.08000000000000002, 'foxes->rabbits': 0.24}
     def __init__(self, state=None):
         self.WorldPloopy = Ploopy()
-        if(state):
-            parseState
-            print(state)
+        self.generations_run = 0
 
     # update loop
     def update(self):
         self.WorldPloopy.update()
+        self.generations_run += 1
+
+    def pprint(self):
+        print("GENERATION: {}".format(self.generations_run))
         self.WorldPloopy.pprint()
 
-    def change(self, relations=None):
+    def replace_edges(self, relations):
         if(relations):
             self.WorldPloopy.replace_edges(relations)
 
     def import_graph(self, g):
         self.WorldPloopy.import_graph(g)
+        if('generations_run' in g):
+            self.generations_run = g['generations_run']
+        else:
+            self.generations_run = 0
+
+    def export_graph(self):
+        g = self.WorldPloopy.export_graph()
+        g['generations_run'] = self.generations_run
+        return g
 
