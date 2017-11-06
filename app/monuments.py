e91f4fb2f4175535610d1121848f3719ffd81867
Francis Tseng <f+accounts@frnsys.com>
2017-11-05 22:50:47 -0500

    fleshed out monuments, composed of topic mixtures, for #17

--- app/monuments.py
+++ app/monuments.py
@@ -1,5 +1,5 @@
-from .params import MONUMENT_GRAPH
 from collections import defaultdict
+from .params import MONUMENT_GRAPH, MONUMENT_TOPICS
 
 
 def sum_dicts(*dicts):
@@ -8,6 +8,17 @@ def sum_dicts(*dicts):
         for k, v in d.items():
             sum[k] += v
     return sum
+
+
+def mixture_update(topic_mixture):
+    """compute a monument state update
+    given a topic mixture"""
+    update = {}
+    for monument, mixture in MONUMENT_TOPICS.items():
+        update[monument] = 0
+        for topic, weight in topic_mixture.items():
+            update[monument] += mixture.get(topic, 0) * weight
+    return update
 
 
 def compute_monuments_state(topic_mixtures):
@@ -23,7 +34,8 @@ def compute_monuments_state(topic_mixtures):
     for tm in topic_mixtures:
         # for each topic and its proportion,
         # update the corresponding monument
-        state = sum_dicts(state, tm)
+        update = mixture_update(tm)
+        state = sum_dicts(state, update)
 
         # then compute how each monument
         # influences each other
