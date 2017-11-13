518f7450f10fc04099d409c964e5ef876403e776
Francis Tseng <f+accounts@frnsys.com>
2017-11-13 15:38:04 -0500

    /planets/<id> to get agg topic mixture and planet color for attendee, closes #33

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from .pp import compute_pp_state
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from .params import MONUMENT_NAMES
+from colorhash import ColorHash
+from collections import defaultdict
 
 app = Flask(__name__)
 CORS(app)
@@ -30,6 +32,14 @@ def get_questions(id):
     return questions
 
 
+def sum_dicts(*dicts):
+    sum = defaultdict(int)
+    for d in dicts:
+        for k, v in d.items():
+            sum[k] += v
+    return sum
+
+
 @app.route('/checkout/<id>', methods=['POST'])
 def checkout(id):
     """records a checkout for a attendee and station"""
@@ -44,6 +54,27 @@ def checkout(id):
     # return book info
     book = LIBRARY['books'][id]
     return jsonify(**book)
+
+
+@app.route('/planets/<id>')
+def planet(id):
+    """returns attendee checkout planet info"""
+    # get topic mixtures for books attendee has checked out
+    topic_mixtures = []
+    for checkout in db['checkouts'].all():
+        if checkout['attendee_id'] == id:
+            book_id = checkout['book_id']
+            topic_mixture = LIBRARY['books'][book_id]['topics']
+            topic_mixtures.append(topic_mixture)
+
+    # sum & normalize
+    topic_mixture = sum_dicts(*topic_mixtures)
+    total = sum(topic_mixture.values())
+    topic_mixture = {t: v/total for t, v in topic_mixture}
+
+    color = ColorHash(id)
+    return jsonify(color=color.hex)
+
 
 
 @app.route('/books')
