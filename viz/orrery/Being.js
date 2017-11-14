f62a93052f35848a9fbae9e18d88a8f2673cce67
Dan Taeyoung <provolot@gmail.com>
2017-11-13 21:27:23 -0500

    orrery kind of working

---
+++ viz/orrery/Being.js
@@ -0,0 +1,152 @@
+import * as THREE from 'three';
+import Boid from './Boid';
+
+// <https://stackoverflow.com/a/13542669/1097920>
+function shadeColor(color, percent) {
+    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}
+
+function uniform(rng) {
+  var [l, u] = rng;
+  return Math.round(l + Math.random() * (u-l));
+}
+
+
+function generateTexture(c1, c2) {
+	var size = 512;
+
+	// create canvas
+	var canvas = document.createElement( 'canvas' );
+	canvas.width = size;
+	canvas.height = size;
+
+	// get context
+	var context = canvas.getContext( '2d' );
+
+	// draw gradient
+	context.rect( 0, 0, size, size );
+	// var gradient = context.createRadialGradient(0,0,0,0,size/2,size);
+	var gradient = context.createLinearGradient( 0, 0, size, size );
+	gradient.addColorStop(0, c1);
+	gradient.addColorStop(0.5, c2);
+	gradient.addColorStop(1, c1);
+	context.fillStyle = gradient;
+	context.fill();
+
+	return canvas;
+}
+
+
+
+class System {
+  constructor(n, herdSizes, geo, color, altitude) {
+    this.boids = [];
+    this.beings = [];
+
+    this.boidGroup = new THREE.Group();
+    this.boidGroup.name = "boidGroup";
+
+
+    for (var i=0; i<n; i++) {
+      var being = new Being(uniform(herdSizes), geo, color, altitude);
+      this.beings.push(being);
+
+      var boid = new Boid({ pos: Boid.randomPos(),
+                            vel: Boid.randomVel(),
+                            rot: Boid.randomRot(),
+                            attr: { color: color,
+                                    name: "Boid-" + i } });
+      this.boids.push(boid);
+      this.boidGroup.add(boid.mesh);
+    }
+
+
+  }
+
+  setVelocities(velocity) {
+    this.beings.forEach(b => b.velocity = velocity);
+  }
+
+  update() {
+    this.boids.forEach(b => b.update(this.boids));
+    this.beings.forEach(b => b.update());
+  }
+}
+
+
+
+
+
+class Being {
+  constructor(popSize, geometry, color, altitude) {
+    this.velocity = {
+      x: 0,
+      y: 0
+    };
+
+    var center = {
+      x: Math.random() * Math.PI*2,
+      y: Math.random() * Math.PI*2
+    };
+    // var material = new THREE.MeshBasicMaterial({color: color});
+    var texture = new THREE.Texture(generateTexture(
+      shadeColor(color, 0.3), shadeColor(color, -0.3)
+    ));
+    texture.needsUpdate = true;
+    var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
+
+    this.group = new THREE.Group();
+    this.herd = [];
+    for (var i=0; i<popSize; i++) {
+      var thisgeo = geometry.clone();
+      thisgeo.applyMatrix( new THREE.Matrix4().makeRotationX(  Math.PI / 2 ) );
+      var member = new THREE.Mesh(thisgeo, material);
+      var box = new THREE.Box3().setFromObject(member);
+      var height = box.size().y;
+      member.s = new THREE.Spherical(
+        1+altitude+height/2,
+        center.x + (Math.random() - 1)/8,
+        center.y + (Math.random() - 1)/8);
+      member.position.setFromSpherical(member.s);
+      // var axesHelper = new THREE.AxesHelper(0.1);
+      // member.add(axesHelper);
+      this.herd.push(member);
+      this.group.add(member);
+    }
+  }
+
+  update() {
+    if (this.velocity.x || this.velocity.y) {
+      this.herd.forEach(member => {
+        member.s.phi += this.velocity.x;
+        member.s.theta += this.velocity.y;
+        member.position.setFromSpherical(member.s);
+      });
+
+      var o = new THREE.Vector3(0,0,0);
+      var newDir = new THREE.Vector3();
+      var quaternion = new THREE.Quaternion();
+      var rotationAxis = new THREE.Vector3(0,0,1);
+      this.herd.forEach(member => {
+
+
+
+        var futureS = new THREE.Spherical(member.s.radius, member.s.phi + (this.velocity.x * 1), member.s.theta + (this.velocity.y * 1));
+        var futureV = new THREE.Vector3();
+        futureV.setFromSpherical(futureS);
+        member.lookAt(futureV);
+
+        //var curDir = member.getWorldDirection();
+        //newDir.subVectors(member.position, o).normalize();
+        //quaternion.setFromUnitVectors(curDir, newDir);
+        //member.applyQuaternion(quaternion);
+      });
+    }
+  }
+}
+
+export { // without default
+    System,
+    Being,
+}
---
+++ viz/orrery/main.js
@@ -0,0 +1,155 @@
+import $ from 'jquery';
+import * as THREE from 'three';
+import OrbitControls from './orbit';
+import {System, Being} from './Being';
+import Boid from './Boid';
+
+var timer = new Timer();
+const CAMERATYPE = 'persp'; // or 'ortho'
+
+
+function choice(choices) {
+  var idx = Math.floor(Math.random() * choices.length);
+  return choices[idx];
+}
+
+function Timer(callback, delay) {
+  var timerId, start, remaining = delay;
+  this.pause = function() {
+    window.clearTimeout(timerId);
+    remaining -= new Date() - start;
+    this.paused = true;
+  };
+  this.resume = function() {
+    start = new Date();
+    window.clearTimeout(timerId);
+    timerId = window.setTimeout(callback, remaining);
+    this.paused = false;
+  };
+  this.resume();
+}
+
+
+
+
+class Orrery {
+  constructor() {
+    this._setupScene();
+    this._setupLights();
+
+    var geometry = new THREE.SphereGeometry(1, 32, 32);
+    // var material = new THREE.MeshBasicMaterial({color: 0x93bcff});
+		var material = new THREE.MeshPhongMaterial({color: 0x93bcff, shading: THREE.FlatShading, wireframe: true});
+
+    this.populate();
+
+    this._setupUI();
+    // var axesHelper = new THREE.AxesHelper( 5 );
+    // this.scene.add( axesHelper );
+  }
+
+  populate() {
+    this.systems = [];
+    var birds = new System(20, [8,16],
+      new THREE.ConeGeometry(0.01, 0.23, 4),
+      '#f3ff21', 0.1);
+    birds.beings.forEach(b => {
+      b.velocity = {
+        x: (Math.random() - 1)/200,
+        y: (Math.random() - 1)/200
+      }
+    });
+    this.scene.add(birds.boidGroup);
+    this.systems.push(birds);
+    this.systems.forEach(s => s.beings.forEach(b => this.scene.add(b.group)));
+    window.birds = birds;
+    window.THREE = THREE;
+  }
+
+  render() {
+    requestAnimationFrame(this.render.bind(this));
+    this.renderer.render(this.scene, this.camera);
+		this.systems.forEach(s => s.update());
+  }
+
+  _setupScene() {
+    var width = window.innerWidth,
+        height = window.innerHeight,
+        aspect = width/height,
+        D = 1;
+
+    this.renderer = new THREE.WebGLRenderer({
+      alpha: true,
+      antialias: false,
+      canvas: document.getElementById('orrery')
+    });
+    this.renderer.setClearColor(0xffffff, 0);
+    this.renderer.setSize(width, height);
+
+    this.scene = new THREE.Scene();
+    if (CAMERATYPE === 'persp') {
+      this.camera = new THREE.PerspectiveCamera(45, aspect, .1, 20000);
+      this.camera.zoom = 2;
+    } else {
+      this.camera = new THREE.OrthographicCamera(-D*aspect, D*aspect, D, -D, 1, 1000),
+      this.camera.zoom = 0.08;
+    }
+
+    this.camera.position.set(-5, 5, 5);
+    this.camera.lookAt(this.scene.position);
+    this.camera.updateProjectionMatrix();
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableZoom = false; // to keep speech bubble more consistently placed
+
+    var self = this;
+    window.addEventListener('resize', function() {
+      var width = window.innerWidth,
+          height = window.innerHeight;
+      self.camera.aspect = width/height;
+      self.camera.updateProjectionMatrix();
+      self.renderer.setSize(width, height);
+    }, false);
+  
+
+  }
+
+  _setupUI() {
+    this.tooltip = document.getElementById('tooltip')
+
+    this.renderer.domElement.addEventListener('mousemove', ev => {
+      var mouse = {
+        x: (ev.clientX/this.renderer.domElement.clientWidth) * 2 - 1,
+        y: -(ev.clientY/this.renderer.domElement.clientHeight) * 2 + 1
+      };
+      var raycaster = new THREE.Raycaster();
+          raycaster.setFromCamera(mouse, this.camera);
+
+      var intersects = raycaster.intersectObjects(this.scene.getObjectByName( "boidGroup" , true).children, true);
+      if (intersects.length > 0) {
+        var obj = intersects[0].object;
+        this.tooltip.innerHTML = this.names[obj.uuid];
+        this.tooltip.style.left = `${ev.clientX + 20}px`;
+        this.tooltip.style.top = `${ev.clientY}px`;
+        this.tooltip.style.display = 'block';
+      } else {
+        this.tooltip.style.display = 'none';
+      }
+    }, false);
+  }
+
+
+
+
+  _setupLights() {
+    var pointLight = new THREE.PointLight(0xffffff, 0.3, 50);
+    pointLight.position.set(0, 20, 0);
+    this.scene.add(pointLight);
+    this.scene.add(new THREE.AmbientLight(0xffffff, 0.75));
+    this.scene.add(new THREE.HemisphereLight(0xCCF0FF, 0xFFA1C7, 0.3));
+  }
+}
+
+var orrery = new Orrery();
+window.orrery = orrery;
+orrery.render();
