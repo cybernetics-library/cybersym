3dc7c4fc898f58ef07f984aa8a9f1f10a7b617a3
Francis Tseng <f+accounts@frnsys.com>
2017-11-15 17:56:34 -0500

    planet name fix

--- app/planets.py
+++ app/planets.py
@@ -1,8 +1,21 @@
+import pandas as pd
+
 ADJS = [l.strip() for l in open('data/adjectives.txt', 'r').readlines() if l.strip()]
 NOUNS = [l.strip() for l in open('data/nouns.txt', 'r').readlines() if l.strip()]
 
+EXISTING = pd.read_csv('qrcode_output.csv')
+EXISTING = {id: name for i, (id, name) in EXISTING[['qrid', 'planet_name']].iterrows()}
+
 def name_from_id(id):
-    h = hash(id)
+    """this will deterministically return
+    a planet name given a unique hex id.
+    note that the first version of this was not actually
+    deterministic, so we also manually lookup ids that were
+    generated then."""
+    if id in EXISTING:
+        return EXISTING[id]
+
+    h = int(id, 16)
     i = h % len(ADJS)
     adj = ADJS[i]
 
