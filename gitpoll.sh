3fb04748a3b3c4e563f3d002a51be33397696ded
Francis Tseng <f+accounts@frnsys.com>
2017-11-17 19:23:52 -0500

    some gitpoll fixes

--- gitpoll.sh
+++ gitpoll.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
+API_PID=""
+VIZ_PID=""
+
+function down {
+    echo "killing..."
+    kill $API_PID
+    kill -9 -$VIZ_PID
+    exit 0
+}
+
+function up {
+    echo "starting..."
+    python app.py >> /tmp/api.log 2>&1 &
+    API_PID=$!
+
+    cd viz
+    npm start >> /tmp/viz.log 2>&1 &
+    VIZ_PID=$!
+    cd ..
+}
+
+trap down INT TERM QUIT EXIT
+
+up
+
 while true; do
+    echo "checking..."
     git fetch
     LOCAL=$(git rev-parse HEAD)
     REMOTE=$(git rev-parse @{u})
     if [ $LOCAL != $REMOTE ]; then
-        if [ ! -z "$1" ]; then
-            kill $API_PID
-            kill $VIZ_PID
-        fi
-
+        echo "updated!"
+        down
         git pull
-
-        python app.py >> /tmp/api.log 2>&1 &
-        API_PID=$!
-
-        cd viz
-        npm start >> /tmp/viz.log 2>&1 &
-        VIZ_PID=$!
-        cd ..
     fi
+    sleep 2
 done
